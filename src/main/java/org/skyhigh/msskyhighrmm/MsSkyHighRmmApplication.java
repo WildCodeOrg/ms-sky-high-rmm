646b11432a0b7ab2aaeba58ec10c8a2f3fe8ef9d
Daniil Popov <popovdanin@gmail.com>
2024-03-03 17:10:23 +0300

    Initial local commit

---
+++ src/main/java/org/skyhigh/msskyhighrmm/MsSkyHighRmmApplication.java
@@ -0,0 +1,13 @@
+package org.skyhigh.msskyhighrmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSkyHighRmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSkyHighRmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skyhigh/msskyhighrmm/MsSkyHighRmmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyhigh.msskyhighrmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSkyHighRmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
