a08fff48afd50810a5834d82a29ef16c5f0eb6af
Daniil Popov <popovdanin@gmail.com>
2024-05-05 22:57:54 +0300

    dvpopov_updates branch changes:  fixed deployment to amvera bug in application.properties 4

--- src/test/java/org/skyhigh/msskyhighrmm/MsSkyHighRmmApplicationTests.java
+++ src/test/java/org/skyhigh/msskyhighrmm/MsSkyHighRmmApplicationTests.java
@@ -5,11 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest
 class MsSkyHighRmmApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
