a2c8b28c83a01872af594fa0c77f99a7633d1686
Daniil Popov <popovdanin@gmail.com>
2024-05-20 19:17:31 +0300

    dvpopov_updates branch changes:  refactored db usage

--- src/main/java/org/skyhigh/msskyhighrmm/repository/UniversalUserRepository.java
+++ src/main/java/org/skyhigh/msskyhighrmm/repository/UniversalUserRepository.java
@@ -16,9 +16,7 @@ public interface UniversalUserRepository extends JpaRepository<UniversalUserEnti
     List<UniversalUserEntity> findByLogin(String login);
 
     //поиск по причине блокировки + имени + фамилии + возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE block_reason_id=?1 AND user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?2 AND user_info ->> 'secondName' = ?3 " +
-            "AND user_info ->> 'age' = ?4", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_block_reason_id_and_first_name_and_second_name_and_age(?1, ?2, ?3, ?4)", nativeQuery = true)
     List<UniversalUserEntity> findByBlockReasonIdANDFirstNameANDSecondNameANDAge(
             String block_reason_id,
             String firstName,
@@ -27,8 +25,7 @@ public interface UniversalUserRepository extends JpaRepository<UniversalUserEnti
     );
 
     //поиск по причине блокировки + имени + фамилии
-    @Query(value = "SELECT * FROM universal_user WHERE block_reason_id=?1 AND user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?2 AND user_info ->> 'secondName' = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_block_reason_id_and_first_name_and_second_name(?1, ?2, ?3)", nativeQuery = true)
     List<UniversalUserEntity> findByBlockReasonIdANDFirstNameANDSecondName(
             String block_reason_id,
             String firstName,
@@ -36,9 +33,7 @@ public interface UniversalUserRepository extends JpaRepository<UniversalUserEnti
     );
 
     //поиск по имени + фамилии + возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?1 AND user_info ->> 'secondName' = ?2 " +
-            "AND user_info ->> 'age' = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_first_name_ans_second_name_and_age(?1, ?2, ?3)", nativeQuery = true)
     List<UniversalUserEntity> findByFirstNameANDSecondNameANDAge(
             String firstName,
             String secondName,
@@ -46,9 +41,7 @@ public interface UniversalUserRepository extends JpaRepository<UniversalUserEnti
     );
 
     //поиск по причине блокировки + фамилии + возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE block_reason_id=?1 AND user_info IS NOT NULL " +
-            "AND user_info ->> 'secondName' = ?2 " +
-            "AND user_info ->> 'age' = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_block_reason_id_and_second_name_and_age(?1, ?2, ?3)", nativeQuery = true)
     List<UniversalUserEntity> findByBlockReasonIdANDSecondNameANDAge(
             String block_reason_id,
             String secondName,
@@ -56,9 +49,7 @@ public interface UniversalUserRepository extends JpaRepository<UniversalUserEnti
     );
 
     //поиск по причине блокировки + имени + возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE block_reason_id=?1 AND user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?2 " +
-            "AND user_info ->> 'age' = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_block_reason_id_and_first_name_and_age(?1, ?2, ?3)", nativeQuery = true)
     List<UniversalUserEntity> findByBlockReasonIdANDFirstNameANDAge(
             String block_reason_id,
             String firstName,
@@ -66,70 +57,61 @@ public interface UniversalUserRepository extends JpaRepository<UniversalUserEnti
     );
 
     //поиск по причине блокировки + имени
-    @Query(value = "SELECT * FROM universal_user WHERE block_reason_id=?1 AND user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_block_reason_id_and_first_name(?1, ?2)", nativeQuery = true)
     List<UniversalUserEntity> findByBlockReasonIdANDFirstName(
             String block_reason_id,
             String firstName
     );
 
     //поиск по причине блокировки + фамилии
-    @Query(value = "SELECT * FROM universal_user WHERE block_reason_id=?1 AND user_info IS NOT NULL " +
-            "AND user_info ->> 'secondName' = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_block_reason_id_and_second_name(?1, ?2)", nativeQuery = true)
     List<UniversalUserEntity> findByBlockReasonIdANDSecondName(
             String block_reason_id,
             String secondName
     );
 
     //поиск по причине блокировки + возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE block_reason_id=?1 AND user_info IS NOT NULL " +
-            "AND user_info ->> 'age' = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_block_reason_id_and_age(?1, ?2)", nativeQuery = true)
     List<UniversalUserEntity> findByBlockReasonIdANDAge(
             String block_reason_id,
             int age
     );
 
     //поиск по имени + фамилии
-    @Query(value = "SELECT * FROM universal_user WHERE user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?1 AND user_info ->> 'secondName' = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_first_name_and_second_name(?1, ?2)", nativeQuery = true)
     List<UniversalUserEntity> findByFirstNameANDSecondName(
             String firstName,
             String secondName
     );
 
     //поиск по имени + возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?1 AND user_info ->> 'age' = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_first_name_and_age(?1, ?2)", nativeQuery = true)
     List<UniversalUserEntity> findByFirstNameANDAge(
             String firstName,
             int age
     );
 
     //поиск по фамилии + возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE user_info IS NOT NULL " +
-            "AND user_info ->> 'secondName' = ?1 AND user_info ->> 'age' = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_second_name_and_age(?1, ?2)", nativeQuery = true)
     List<UniversalUserEntity> findBySecondNameANDAge(
             String secondName,
             int age
     );
 
     //поиск по имени
-    @Query(value = "SELECT * FROM universal_user WHERE user_info IS NOT NULL " +
-            "AND user_info ->> 'firstName' = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_first_name(?1)", nativeQuery = true)
     List<UniversalUserEntity> findByFirstName(
             String firstName
     );
 
     //поиск по фамилии
-    @Query(value = "SELECT * FROM universal_user WHERE user_info IS NOT NULL " +
-            "AND user_info ->> 'secondName' = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_second_name(?1)", nativeQuery = true)
     List<UniversalUserEntity> findBySecondName(
             String secondName
     );
 
     //поиск по возрасту
-    @Query(value = "SELECT * FROM universal_user WHERE user_info IS NOT NULL " +
-            "AND user_info ->> 'age' = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM find_by_age(?1)", nativeQuery = true)
     List<UniversalUserEntity> findByAge(
             int age
     );
