f11c5c5afc9ae2bd53acd6dce4241b2553422207
Daniil Popov <popovdanin@gmail.com>
2024-08-21 16:30:54 +0300

    dvpopov_updates branch changes: added eureka client connection and refactored WebSecurityConfig

--- src/main/java/org/skyhigh/msskyhighrmm/MsSkyHighRmmApplication.java
+++ src/main/java/org/skyhigh/msskyhighrmm/MsSkyHighRmmApplication.java
@@ -3,7 +3,9 @@ package org.skyhigh.msskyhighrmm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MsSkyHighRmmApplication {
 
--- src/main/java/org/skyhigh/msskyhighrmm/config/WebSecurityConfig.java
+++ src/main/java/org/skyhigh/msskyhighrmm/config/WebSecurityConfig.java
@@ -2,17 +2,42 @@ package org.skyhigh.msskyhighrmm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.logging.Logger;
 
 @Configuration
-public class WebSecurityConfig  {
+@EnableWebSecurity
+public class WebSecurityConfig {
     private static final Logger log = Logger.getLogger(WebSecurityConfig.class.getName());
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(8);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
     }
 }
